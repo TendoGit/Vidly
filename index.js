945e56e56e8aa669dd1629fc1d6e4d597096f7bb
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-21 14:51:26 +0200

    Genres API: Handling HTTP POST Requests

--- index.js
+++ index.js
@@ -22,5 +22,26 @@ app.get('/api/genres/:id', (req, res) => {
     res.send(genre);
 });
 
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+
+    genres.push(genre);
+    res.send(genre);
+});
+
+function validateGenre(genre) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(genre);
+}
+
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
