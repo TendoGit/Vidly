76cebd20cf4f4903724ebe07a99f501d9fc79c6f
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-21 15:00:19 +0200

    Genres API: Handling HTTP DELETE Requests

--- index.js
+++ index.js
@@ -46,6 +46,16 @@ app.put('/api/genres/:id', (req, res) => {
     res.send(genre);
 });
 
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find( g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    res.send(genre);
+});
+
 function validateGenre(genre) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
