da8ed07d5ff5a50fcfc537aa74dcf69e7e0d690a
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-21 14:56:53 +0200

    Genres API: Handling HTTP PUT Requests

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ app.post('/api/genres', (req, res) => {
     res.send(genre);
 });
 
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find( g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genres.name = req.body.name;
+    res.send(genre);
+});
+
 function validateGenre(genre) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
