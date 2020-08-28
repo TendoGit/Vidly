546847ae64c51e6f323d0b3ae4450971ef98973d
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-28 16:37:56 +0200

    added a new module: Config

--- index.js
+++ index.js
@@ -1,68 +1,11 @@
+const config = require('config');
 const Joi = require('joi');
 const express =  require('express');
 const app = express();
+const genres = require('./routes/genres');
 
 app.use(express.json());
-
-const genres = [
-    {id: 1, name: "Thriller"},
-    {id: 1, name: "Sci-Fi"},
-    {id: 1, name: "Romance"},
-    {id: 1, name: "Other"}
-]
-
-app.get('/api/genres', (req, res) => {
-    res.send(genres);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-    
-    res.send(genre);
-});
-
-app.post('/api/genres', (req, res) => {
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    };
-
-    genres.push(genre);
-    res.send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    genres.name = req.body.name;
-    res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find( g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    res.send(genre);
-});
-
-function validateGenre(genre) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    return schema.validate(genre);
-}
+app.use('/api/genres', genres);
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
