b7a47d49d7f1bed6176db277977f87196e1fa63a
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-28 16:43:48 +0200

    added a new module: Mongoose

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const mongoose = require('mongoose');
 const config = require('config');
 const Joi = require('joi');
 const express =  require('express');
 const app = express();
 const genres = require('./routes/genres');
+
+mongoose.connect(config.get('dbConn'))
+    .then(console.log('Connected to MongoDB..'))
+    .catch(err => console.log('Could not connect to MongoDB..', err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
