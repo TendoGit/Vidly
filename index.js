1ad584eedfa9e3dbbd568cbb0ff71f40cff48631
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-28 17:01:57 +0200

    addded persistence to Genre API

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const Joi = require('joi');
 const express =  require('express');
 const app = express();
 const genres = require('./routes/genres');
