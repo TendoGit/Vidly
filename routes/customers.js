3f7f705f33af5b4a489ad50fd5f3694e967592e4
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-31 11:58:15 +0200

    Customer API: Handling HTTP PUT requests

--- routes/customers.js
+++ routes/customers.js
@@ -47,6 +47,22 @@ router.post('/', async (req, res) => {
     res.send(customer)
 });
 
+router.put('/:id', async (req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+        isGold: req.body.isGold,
+        name: req.body.name,
+        phone: req.body.phone
+    }, 
+    {new: true});
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found');
+
+    res.send(customer)
+});
+
 function validateCustomer(customer) {
     const schema = Joi.object({
         isGold: Joi.boolean().required(),
