2109dc93935805aecac0ecd767570578e0fca681
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-31 14:00:43 +0200

    Refactoring: Genres Model

---
+++ models/genres.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+}));
+
+function validateGenre(genre) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(genre);
+}
+
+exports.Genre = Genre;
+exports.validate = validateGenre;
--- routes/genres.js
+++ routes/genres.js
@@ -1,14 +1,7 @@
-const Joi = require('joi');
+const { Genre, validate } = require('../models/genres');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    }
-}));
 
 router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name');
@@ -24,7 +17,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const { error } = validateGenre(req.body);
+    const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     let genre = new Genre({ name: req.body.name });
@@ -33,7 +26,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-    const { error } = validateGenre(req.body);
+    const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     
     const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, { new: true });
@@ -50,13 +43,5 @@ router.delete('/:id', async (req, res) => {
 
     res.send(genre);
 });
-
-function validateGenre(genre) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    return schema.validate(genre);
-}
 
 module.exports = router;
