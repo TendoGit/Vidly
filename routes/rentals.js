810c46c4ca5b1fe21e71e51e7df68ce8feac15e4
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-31 23:23:13 +0200

    Rentals API: Handling HTTP POST requests

--- routes/rentals.js
+++ routes/rentals.js
@@ -17,5 +17,38 @@ router.get('/:id', async (req, res) => {
     res.send(rental);
 });
 
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-module.exports = router;
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer) return res.status(400).send('Invalid customer.');
+
+    const movie = await Movie.findById(req.body.movieId);
+    if (!movie) return res.status(400).send('Invalid movie.');
+
+    if (movie.numberInStock === 0) return res.status(400).send('Movie out of stock');
+
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            isGold: customer.isGold,
+            phone: customer.phone
+        },
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate
+        }
+    });
+
+    rental = await rental.save();
+
+    movie.numberInStock--;
+    movie.save();
+
+    res.send(rental);
+});
+
+module.exports = router;  
