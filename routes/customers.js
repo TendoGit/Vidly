487fa548c5861565fdf1af67398b3826c5a77467
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-31 11:52:31 +0200

    Customer API: Handling HTTP POST requests

--- routes/customers.js
+++ routes/customers.js
@@ -33,6 +33,20 @@ router.get('/:id', async (req, res) => {
     res.send(customer);
 });
 
+router.post('/', async (req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let customer = new Customer({
+        isGold: req.body.isGold,
+        name: req.body.name,
+        phone: req.body.phone
+    });
+    customer = await customer.save();
+
+    res.send(customer)
+});
+
 function validateCustomer(customer) {
     const schema = Joi.object({
         isGold: Joi.boolean().required(),
