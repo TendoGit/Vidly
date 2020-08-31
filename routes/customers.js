164e0c6d3fbe78db97df88f09d27099f2233843a
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-31 12:01:31 +0200

    Customer API: Handling HTTP DELETE requests

--- routes/customers.js
+++ routes/customers.js
@@ -63,6 +63,14 @@ router.put('/:id', async (req, res) => {
     res.send(customer)
 });
 
+router.delete('/:id', async (req, res) => {
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+    
+    if (!customer) return res.status(404).send('The customer with the given ID was not found');
+    
+    res.send(customer);
+});
+
 function validateCustomer(customer) {
     const schema = Joi.object({
         isGold: Joi.boolean().required(),
