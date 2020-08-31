be26d3b940e1d3fee5dd2c992debc8b50e2f7dd5
Tendai B. Mandaza <tendai.mandaza@gmail.com>
2020-08-31 20:08:05 +0200

    Rentals API: created a rentals model

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+
 
 mongoose.connect(config.get('dbConn'))
     .then(console.log('Connected to MongoDB..'))
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
